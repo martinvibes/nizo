d216d052e20e2c643ecdd67717301ccfc565fb22
yunusabdul <yunusabdulmajidyunus38@gmail.com>
2024-12-22 10:32:17 +0100

    fix: dashboard

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="md:p-2">
+      <body>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
