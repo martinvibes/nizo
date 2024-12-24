7f4176a43e43989dab2b3499c2f2941f4804bc4a
yunusabdul <yunusabdulmajidyunus38@gmail.com>
2024-12-24 12:24:07 +0100

    wallet balance

--- src/hook/useGetBalance.tsx
+++ src/hook/useGetBalance.tsx
@@ -4,21 +4,22 @@ import { useEffect, useState } from "react";
 
 export function useGetBalance() {
   const { publicKey } = useWallet();
-  const [balance, setBalance] = useState(0);
+  const [balance, setBalance] = useState<number|null>(null);
   useEffect(() => {
     if (!publicKey) {
-      setBalance(0); // Wallet not connected
+      setBalance(null); // Wallet not connected
       return;
     }
 
     // Create a connection to the Solana network
-    const connection = new Connection("https://api.devnet.solana.com");
+    const connection = new Connection("","confirmed");
 
     const fetchBalance = async () => {
       try {
-        const balance = await connection.getAccountInfo(publicKey);
-        console.log(balance);
-        //setBalance(balance / 1e9); // Convert lamports to SOL
+        const balance = await connection.getBalance(publicKey);
+        // set to 3 decimal place
+        const balanceDecimal = +balance/1e9
+        setBalance(balanceDecimal); // Convert lamports to SOL
       } catch (error) {
         console.error("Error fetching balance:", error);
       }
